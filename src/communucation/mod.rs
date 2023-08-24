f03d21299217b8c522f1e952dd1ea15aaa41fcfc
Yigit Ozcelep <yoroso85onurlu@outlook.com>
2023-08-24 05:04:32 +0300

    change name of communucation files

--- src/communucation/mod.rs
+++ src/communucation/mod.rs
@@ -1,2 +1,2 @@
-pub mod backend;
-pub mod frontend;
+pub mod send;
+pub mod recieve;
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,3 @@ pub mod game;
 pub mod communucation;
 
 
-fn add(left: usize, right: usize) -> usize {
-    left + right
-}
